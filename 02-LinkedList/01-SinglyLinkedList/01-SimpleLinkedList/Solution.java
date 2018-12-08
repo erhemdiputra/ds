d4d947a743c15dae41d2a83c9d9caad70c43531c
erhemdiputra <erhem.diputra@tokopedia.com>
2018-12-08 15:47:21 +0700

    initial commit

---
+++ 02-LinkedList/01-SinglyLinkedList/01-SimpleLinkedList/Solution.java
@@ -0,0 +1,54 @@
+public class Solution {
+    Node head;
+    static class Node {
+        int data;
+        Node next;
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args)  {
+        Solution llist = new Solution();
+
+        llist.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        /* Three nodes have been allocated  dynamically. 
+          We have refernces to these three blocks as first,   
+          second and third 
+  
+          llist.head        second              third 
+             |                |                  | 
+             |                |                  | 
+         +----+------+     +----+------+     +----+------+ 
+         | 1  | null |     | 2  | null |     |  3 | null | 
+         +----+------+     +----+------+     +----+------+ */
+  
+        llist.head.next = second; // Link first node with the second node 
+
+         /*  Now next of first Node refers to second.  So they 
+             both are linked. 
+   
+          llist.head        second              third 
+             |                |                  | 
+             |                |                  | 
+         +----+------+     +----+------+     +----+------+ 
+         | 1  |  o-------->| 2  | null |     |  3 | null | 
+         +----+------+     +----+------+     +----+------+ */
+   
+        second.next = third; // Link second node with the third node 
+
+         /*  Now next of second Node refers to third.  So all three 
+             nodes are linked. 
+   
+          llist.head        second              third 
+             |                |                  | 
+             |                |                  | 
+         +----+------+     +----+------+     +----+------+ 
+         | 1  |  o-------->| 2  |  o-------->|  3 | null | 
+         +----+------+     +----+------+     +----+------+ */
+    } 
+} 
